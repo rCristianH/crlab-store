aa6c29338f6887b6c400271c456eafc5049d15c5
Cristian Ramirez <ccrhdbj@outlook.com>
2023-03-27 18:27:53 -0500

    Add shopping cart functionality and toggle aside element on click in main.js and update styles in style.css

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,21 @@
-const menuEmail = document.querySelector(".navbar-email")
-const desktopMenu = document.querySelector('.desktop-menu')
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail-shopping-cart");
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleCartAside)
 
-function toggleDesktopMenu(){
-    console.log("toogle")
-    desktopMenu.classList.toggle('inactive')
+function toggleDesktopMenu() {
+  console.log("toogle");
+  desktopMenu.classList.toggle("inactive");
 }
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+}
+function toggleCartAside(){
+    aside.classList.toggle('inactive')
 }
