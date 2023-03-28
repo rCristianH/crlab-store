27dbadd94959d744ac91b3aeedf8bd7b4918c1f5
Cristian Ramirez <ccrhdbj@outlook.com>
2023-03-27 19:18:50 -0500

    Refactor mobile menu and order detail display in CSS and HTML

--- assets/main.js
+++ assets/main.js
@@ -5,27 +5,44 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const menuCartIcon = document.querySelector(".navbar-shopping-cart");
 const aside = document.querySelector(".product-detail-shopping-cart");
 
+
+//funciones de apoyo
+function desktopMenuClosed() {
+  const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+  if (!isDesktopMenuClosed) {
+    desktopMenu.classList.add("inactive");
+  }
+}
+function mobileMenuClosed() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+  }
+}
+function asideClosed() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+}
+//funciones de apoyo
+
+
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCartIcon.addEventListener("click", toggleCartAside);
 
 function toggleDesktopMenu() {
-  console.log("toogle");
+  asideClosed();
   desktopMenu.classList.toggle("inactive");
 }
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains("inactive");
-  if (!isAsideClosed) {
-    aside.classList.add("inactive");
-  }
+  asideClosed();
   mobileMenu.classList.toggle("inactive");
 }
 function toggleCartAside() {
-  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
-
-  if (!isMobileMenuClosed) {
-    mobileMenu.classList.add("inactive");
-  }
-
+  mobileMenuClosed();
+  desktopMenuClosed();
   aside.classList.toggle("inactive");
 }
