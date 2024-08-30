aef5819ebda03ba2e4836be9555ca7f239c171b9
Cristian Ramirez <ccrhdbj@outlook.com>
2024-08-30 18:16:50 -0500

    test tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 function App() {
-
   return (
-    <></>
-  )
+    <>
+      <div className="bg-red-100">Hello World, little friend</div>
+    </>
+  );
 }
 
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("../sw.js").then(
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('../sw.js').then(
       function (registration) {
         console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
+          'ServiceWorker registration successful with scope: ',
+          registration.scope,
         );
       },
       function (err) {
-        console.log("ServiceWorker registration failed: ", err);
-      }
+        console.log('ServiceWorker registration failed: ', err);
+      },
     );
   });
 }
 
-
-export default App
+export default App;
