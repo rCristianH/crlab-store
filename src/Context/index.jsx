a630112bbda3210306405f9dbbefff7b35895985
Cristian Ramirez <ccrhdbj@outlook.com>
2024-09-09 11:37:41 -0500

    global context

---
+++ src/Context/index.jsx
@@ -0,0 +1,11 @@
+import { createContext } from 'react';
+
+const ShoppingCartContext = createContext();
+
+const ShoppingCartProvider = ({ children }) => {
+  return (
+    <ShoppingCartContext.Provider>{children}</ShoppingCartContext.Provider>
+  );
+};
+
+export { ShoppingCartProvider };
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { serviceWorker } from '../../Utils/SW';
 import { AppRoutes } from '../../Routes/AppRoutes';
 import { Navbar } from '../../Components/Navbar';
+import { ShoppingCartProvider } from '../../Context';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRoutes />
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   );
 };
 
