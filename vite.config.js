50ea5bb503da5527144563e44de175e79eb6bf5b
Cristian Ramirez <ccrhdbj@outlook.com>
2024-10-10 12:29:46 -0500

    change base !IMPORTANT

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: mode === "production" ? '/front-store/' : '/',
+    base: '/front-store/',
     plugins: [
       react(),
       VitePWA({
