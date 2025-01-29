27bd15595ca4d802ed2a6d3c82729e63554ba798
Cristian Ramirez <ccrhdbj@outlook.com>
2025-01-29 13:00:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: '/crlab-store/',
     plugins: [
       react(),
       VitePWA({
